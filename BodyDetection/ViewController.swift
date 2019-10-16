fe53780356409cbff344bf74b840dfb62c10b3f6
Mateus Nunes <nunes.mateus@pucpr.edu.br>
2019-10-16 11:10:51 -0300

    Version

--- BodyDetection/ViewController.swift
+++ BodyDetection/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         
         // Asynchronously load the 3D character.
         var cancellable: AnyCancellable? = nil
-        cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
+        cancellable = Entity.loadBodyTrackedAsync(named: "character/hulk_agora_vai").sink(
             receiveCompletion: { completion in
                 if case let .failure(error) = completion {
                     print("Error: Unable to load model: \(error.localizedDescription)")
@@ -51,8 +51,9 @@ class ViewController: UIViewController, ARSessionDelegate {
                 cancellable?.cancel()
         }, receiveValue: { (character: Entity) in
             if let character = character as? BodyTrackedEntity {
-                // Scale the character to human size
-                character.scale = [1.0, 1.0, 1.0]
+                //Tamanho do boneco em relacão a humanos
+                character.scale = [0.03,0.03,0.03]
+//                character.jointNames.map({m in return print(m)})
                 self.character = character
                 cancellable?.cancel()
             } else {
@@ -60,22 +61,25 @@ class ViewController: UIViewController, ARSessionDelegate {
             }
         })
     }
-    
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         for anchor in anchors {
             guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
             
             // Update the position of the character anchor's position.
             let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
+            
             characterAnchor.position = bodyPosition + characterOffset
+            print("characterOffSet  \(characterOffset)")
+            print("bodyPosition   \(bodyPosition)")
+            print("posicaoCharacterAncora \(characterAnchor.position)")
+            print("_______________________________")
+          
             // Also copy over the rotation of the body anchor, because the skeleton's pose
             // in the world is relative to the body anchor's rotation.
             characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
    
             if let character = character, character.parent == nil {
-                // Attach the character to its anchor as soon as
-                // 1. the body anchor was detected and
-                // 2. the character was loaded.
+                print("Corpo ancora detectado ")
                 characterAnchor.addChild(character)
             }
         }
